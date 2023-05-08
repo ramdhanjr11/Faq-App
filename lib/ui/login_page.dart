c44b097a16dd6f472b38301d6af050a7198f4da3
Muramsyah <ramdhanjr690@gmail.com>
2023-05-08 16:47:59 +0700

    core[login]: add form login ui

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,6 +10,15 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  bool _isVisible = false;
+
+  void _setPasswordVisible() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +32,82 @@ class _LoginPageState extends State<LoginPage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 16),
-              Text(
-                'Welcome to FAQ App',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const SizedBox(height: 4),
-              const Text('Please login with your account..'),
-              const SizedBox(height: 24),
-            ],
+          child: FormBuilder(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.disabled,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 16),
+                Text(
+                  'Welcome to FAQ App',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const SizedBox(height: 4),
+                const Text('Please login with your account..'),
+                const SizedBox(height: 64),
+                Text(
+                  'Email',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                const SizedBox(height: 8),
+                FormBuilderTextField(
+                  name: "email",
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'example@gmail.com',
+                    prefixIcon: Icon(Icons.email),
+                    prefixIconColor: Colors.grey,
+                  ),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.email(),
+                  ]),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Password',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                const SizedBox(height: 8),
+                FormBuilderTextField(
+                  name: 'password',
+                  obscureText: _isVisible,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'password',
+                    prefixIcon: const Icon(Icons.key),
+                    prefixIconColor: Colors.grey,
+                    suffixIcon: IconButton(
+                      onPressed: () => _setPasswordVisible(),
+                      icon: _isVisible
+                          ? const Icon(Icons.visibility)
+                          : const Icon(Icons.visibility_off),
+                    ),
+                    suffixIconColor: Colors.grey,
+                  ),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.minLength(
+                      5,
+                      errorText: '5 characters minimum',
+                    ),
+                  ]),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.saveAndValidate()) {
+                        print(_formKey.currentState!.value);
+                      }
+                    },
+                    child: const Text('Login'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
