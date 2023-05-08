dd4c847de44f9a750ce73cd55426b46401a32b1a
Muramsyah <ramdhanjr690@gmail.com>
2023-05-08 23:17:50 +0700

    core: use colorSchemeSeed for material 3

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
-          primarySwatch: Colors.blue,
+          colorSchemeSeed: Colors.green,
         ),
         initialRoute: AppRoutes.loginRouteName,
         routes: {
